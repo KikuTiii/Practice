22a53bbd7f541df20c69eb8aec70ddd7d2135f4f
Matheus Kikuti <111128991+KikuTiii@users.noreply.github.com>
2023-06-03 17:31:28 -0300

    Add files via upload
    learning about tuples

---
+++ Python/tuples.py
@@ -0,0 +1,15 @@
+tuple = (1, 2, 3, 4, 5)
+
+#Tuples can contain elements of different data types, such as integers, floating point numbers, strings, and even other tuples. For example:
+tuple = (1, 2.5, "Olá", (4, 5, 6))
+
+#tuples can also be empty, containing zero elements:
+empty_tuple = ()
+
+#To access the elements of a tuple, you can use indexing, just like lists. For example, to access the first element of the tuple "tuple" from the example above, you can do:
+first_element = tuple[0]
+
+#Additionally, tuples support unpacking, where you can assign the tuple's elements to separate variables. For example:  
+tuple = (1, 2, 3)
+a, b, c = tuple
+#In this case, the variable "a" will receive the value 1, "b" will receive the value 2 and "c" will receive the value 3.
