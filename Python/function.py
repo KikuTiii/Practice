935935db1217751244939b87b1eb83843399d57e
Matheus Kikuti <111128991+KikuTiii@users.noreply.github.com>
2023-06-04 23:26:08 -0300

    Add files via upload

---
+++ Python/function.py
@@ -0,0 +1,8 @@
+def vai_curintia(): #def is the keyword used to define a function.
+    print("vai curintia")
+
+def say_hello(name):
+    print("Hello " + name)
+
+print(say_hello("Kikuti"))
+print(vai_curintia())
